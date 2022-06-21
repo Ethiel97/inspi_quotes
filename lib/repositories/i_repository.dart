84692d12482af562171bcabb117af4003413b519
Ethiel ADIASSA <ethiel97@gmail.com>
2022-06-21 20:16:38 +0000

    refactoring

--- lib/repositories/i_repository.dart
+++ lib/repositories/i_repository.dart
@@ -2,7 +2,7 @@ import 'package:smart_quotes/models/author.dart';
 import 'package:smart_quotes/models/quote.dart';
 import 'package:smart_quotes/models/tag.dart';
 
-abstract class IRrepository {
+abstract class IRrepository<T> {
   Future<List<Quote>> getQuotes({Map<String, dynamic> query = const {}});
 
   Future<List<Author>> getAuthors({Map<String, dynamic> query = const {}});
